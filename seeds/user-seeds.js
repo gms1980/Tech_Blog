5b8c129e591daf9e72260413d7b1f78aea80d690
George <gms1980@icloud.com>
2021-07-01 09:26:41 -0700

    updated code

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -54,6 +54,6 @@ const userData = [
   }
 ];
 
-const seedUsers = () => User.bulkCreate(userData, {individualHooks: true});
+const seedUsers = () => User.bulkCreate(userData);
 
 module.exports = seedUsers;
